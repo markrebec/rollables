53ee73e0096919a9de8f0674d4278a404f4e5160
Mark Rebec <mark@markrebec.com>
2013-01-04 13:31:42 -0800

    pulling up test a level so they make more sense with restructuring

--- spec/rollables/dice_spec.rb
+++ spec/rollables/dice_spec.rb
@@ -140,95 +140,94 @@ describe Rollables::Dice do
     Rollables::Dice.new(1..6, "1d6").to_s.should == "2d6"
     Rollables::Dice.new("2d8", Rollables::Die.new(["a","b","c"]), Rollables::Die.new(["a","b","c"])).to_s.should == "2d8,2d(a,b,c)"
   end
+end
 
-  describe Rollables::DiceCollection do
-    it "should allow adding a single die" do
-      d1 = Rollables::Dice.new
-      d1.dice.add_die(:d6)
-      d1.dice.length.should == 1
-      d2 = Rollables::Dice.new(20)
-      d2.dice.add_die(:d6)
-      d2.dice.length.should == 2
-    end
-
-    it "should allow adding multiple dice" do
-      d1 = Rollables::Dice.new
-      d1.dice.add_dice([:d6, "2d8"])
-      d1.dice.length.should == 3
-      d2 = Rollables::Dice.new(20)
-      d2.dice.add_dice([:d6, "2d8"])
-      d2.dice.length.should == 4
-    end
-    
-    it "should allow chaining the addition of dice" do
-      Rollables::Dice.new.dice.add_dice([6, 12]).add_die(:d8).add_dice("2d20").length.should == 5
-    end
-    
-    it "should be numeric if all die have all numeric faces" do
-      Rollables::Dice.new(:d6, 1..8, 20).dice.numeric?.should be_true
-    end
-
-    it "should not be numeric if any die have any non-numeric faces" do
-      Rollables::Dice.new(:d6, Rollables::Die.new(["a","b","c"]), 20).dice.numeric?.should be_false
-    end
-
-    it "should be sequential if all die are all numeric and sequential" do
-      Rollables::Dice.new(6, 12, Rollables::Die.new([2,5,3,4])).dice.sequential?.should be_true
-    end
-
-    it "should not be sequential if any die are non-numeric or non-sequential" do
-      Rollables::Dice.new(6, Rollables::Die.new([1,2,3,4,"a"])).dice.sequential?.should be_false
-      Rollables::Dice.new(6, :d20, Rollables::Die.new([2,5,4])).dice.sequential?.should be_false
-    end
-
-    it "should be simple if all die are all simple" do
-      Rollables::Dice.new(6, 12, Rollables::Die.new([1,2,5,3,4])).dice.simple?.should be_true
-    end
-
-    it "should not be simple if any die are not simple" do
-      Rollables::Dice.new(6, Rollables::Die.new([1,2,3,4,"a"])).dice.simple?.should be_false
-      Rollables::Dice.new(6, :d20, Rollables::Die.new([2,3,4])).dice.simple?.should be_false
-    end
-    
-    it "should return the correct value(s) for high" do
-      Rollables::Dice.new(8, 20, 6).dice.high.should == 34
-      Rollables::Dice.new(20, Rollables::Die.new([0,5,10])).dice.high.should == 30
-      Rollables::Dice.new(6, 20, Rollables::Die.new(["a",2,3])).dice.high.should == [6,20,3]
-    end
-    
-    it "should return the correct value(s) for low" do
-      Rollables::Dice.new(8, 20, 6).dice.low.should == 3
-      Rollables::Dice.new(20, Rollables::Die.new([0,5,10])).dice.low.should == 1
-      Rollables::Dice.new(6, 20, Rollables::Die.new(["a",2,3])).dice.low.should == [1,1,"a"]
-    end
+describe Rollables::DiceCollection do
+  it "should allow adding a single die" do
+    d1 = Rollables::Dice.new
+    d1.dice.add_die(:d6)
+    d1.dice.length.should == 1
+    d2 = Rollables::Dice.new(20)
+    d2.dice.add_die(:d6)
+    d2.dice.length.should == 2
   end
 
-  describe Rollables::DiceRoll do
-    it "should return a properly formatted string from to_s" do
-      [Rollables::Dice.new(:d8, Rollables::Die.new([1,4,3,5,2])), Rollables::Dice.new(Rollables::Die.new(["a","b","c"]), 6), Rollables::Dice.new(1..12, Rollables::Die.new([2,5,1,9,8]))].each do |dice|
-        roll = dice.roll
-        if (dice.numeric?)
-          roll.to_s.should == "#{roll.rolls.collect { |r| "#{r.die.to_s}=#{r.value.to_s}" }.join(" + ")} = #{roll.value.sum}"
-        else
-          roll.to_s.should == "#{roll.rolls.collect { |r| "#{r.die.to_s}=#{r.value.to_s}" }.join(" + ")} = (#{roll.value.join(",")})"
-        end
-      end
-    end
-    
-    it "should return all the right face values for die rolls" do
-      dice = Rollables::Dice.new("3d6")
+  it "should allow adding multiple dice" do
+    d1 = Rollables::Dice.new
+    d1.dice.add_dice([:d6, "2d8"])
+    d1.dice.length.should == 3
+    d2 = Rollables::Dice.new(20)
+    d2.dice.add_dice([:d6, "2d8"])
+    d2.dice.length.should == 4
+  end
+  
+  it "should allow chaining the addition of dice" do
+    Rollables::Dice.new.dice.add_dice([6, 12]).add_die(:d8).add_dice("2d20").length.should == 5
+  end
+  
+  it "should be numeric if all die have all numeric faces" do
+    Rollables::Dice.new(:d6, 1..8, 20).dice.numeric?.should be_true
+  end
+
+  it "should not be numeric if any die have any non-numeric faces" do
+    Rollables::Dice.new(:d6, Rollables::Die.new(["a","b","c"]), 20).dice.numeric?.should be_false
+  end
+
+  it "should be sequential if all die are all numeric and sequential" do
+    Rollables::Dice.new(6, 12, Rollables::Die.new([2,5,3,4])).dice.sequential?.should be_true
+  end
+
+  it "should not be sequential if any die are non-numeric or non-sequential" do
+    Rollables::Dice.new(6, Rollables::Die.new([1,2,3,4,"a"])).dice.sequential?.should be_false
+    Rollables::Dice.new(6, :d20, Rollables::Die.new([2,5,4])).dice.sequential?.should be_false
+  end
+
+  it "should be simple if all die are all simple" do
+    Rollables::Dice.new(6, 12, Rollables::Die.new([1,2,5,3,4])).dice.simple?.should be_true
+  end
+
+  it "should not be simple if any die are not simple" do
+    Rollables::Dice.new(6, Rollables::Die.new([1,2,3,4,"a"])).dice.simple?.should be_false
+    Rollables::Dice.new(6, :d20, Rollables::Die.new([2,3,4])).dice.simple?.should be_false
+  end
+  
+  it "should return the correct value(s) for high" do
+    Rollables::Dice.new(8, 20, 6).dice.high.should == 34
+    Rollables::Dice.new(20, Rollables::Die.new([0,5,10])).dice.high.should == 30
+    Rollables::Dice.new(6, 20, Rollables::Die.new(["a",2,3])).dice.high.should == [6,20,3]
+  end
+  
+  it "should return the correct value(s) for low" do
+    Rollables::Dice.new(8, 20, 6).dice.low.should == 3
+    Rollables::Dice.new(20, Rollables::Die.new([0,5,10])).dice.low.should == 1
+    Rollables::Dice.new(6, 20, Rollables::Die.new(["a",2,3])).dice.low.should == [1,1,"a"]
+  end
+end
+
+describe Rollables::DiceRoll do
+  it "should return a properly formatted string from to_s" do
+    [Rollables::Dice.new(:d8, Rollables::Die.new([1,4,3,5,2])), Rollables::Dice.new(Rollables::Die.new(["a","b","c"]), 6), Rollables::Dice.new(1..12, Rollables::Die.new([2,5,1,9,8]))].each do |dice|
       roll = dice.roll
-      roll.value.should == roll.rolls.map(&:value)
-    end
-  end
-
-  describe Rollables::DiceRolls do
-    it "should return a properly formatted string from to_s" do
-      [Rollables::Dice.new(:d8, Rollables::Die.new([1,4,3,5,2])), Rollables::Dice.new(Rollables::Die.new(["a","b","c"]), 6), Rollables::Dice.new(1..12, Rollables::Die.new([2,5,1,9,8]))].each do |dice|
-        5.times { dice.roll }
-        dice.rolls.to_s.should == dice.rolls.join(", ")
+      if (dice.numeric?)
+        roll.to_s.should == "#{roll.rolls.collect { |r| "#{r.die.to_s}=#{r.value.to_s}" }.join(" + ")} = #{roll.value.sum}"
+      else
+        roll.to_s.should == "#{roll.rolls.collect { |r| "#{r.die.to_s}=#{r.value.to_s}" }.join(" + ")} = (#{roll.value.join(",")})"
       end
     end
   end
+  
+  it "should return all the right face values for die rolls" do
+    dice = Rollables::Dice.new("3d6")
+    roll = dice.roll
+    roll.value.should == roll.rolls.map(&:value)
+  end
+end
 
+describe Rollables::DiceRolls do
+  it "should return a properly formatted string from to_s" do
+    [Rollables::Dice.new(:d8, Rollables::Die.new([1,4,3,5,2])), Rollables::Dice.new(Rollables::Die.new(["a","b","c"]), 6), Rollables::Dice.new(1..12, Rollables::Die.new([2,5,1,9,8]))].each do |dice|
+      5.times { dice.roll }
+      dice.rolls.to_s.should == dice.rolls.join(", ")
+    end
+  end
 end
--- spec/rollables/die_spec.rb
+++ spec/rollables/die_spec.rb
@@ -170,148 +170,147 @@ describe Rollables::Die do
     Rollables::Die.new(2..4).to_s.should == "d(2,3,4)"
     Rollables::Die.new(["x","y","z"]).to_s.should == "d(x,y,z)"
   end
+end
 
-  describe Rollables::DieRoll do
-    it "should roll on init" do
-      die = Rollables::Die.new(:d6)
-      roll = die.roll
-      roll.value.should_not be_nil
-      roll.timestamp.should_not be_nil
-    end
-
-    it "should return a properly formatted string from to_s" do
-      [Rollables::Die.new(6), Rollables::Die.new(["a","b","c","hello"])].each do |die|
-        20.times do
-          roll = die.roll
-          roll.to_s.should == roll.value.to_s
-        end
-      end
-    end
+describe Rollables::DieFace do
+  it "should return the correct modified instance" do
+    die = Rollables::Die.new([1,"2","z"])
+    die.faces[0].should be_a(Integer)
+    die.faces[1].should be_a(Integer)
+    die.faces[2].should be_a(String)
   end
 
-  describe Rollables::DieRolls do
-    it "should return a properly formatted string from to_s" do
-      [Rollables::Die.new(6), Rollables::Die.new(["z","y","x"])].each do |die|
-        5.times { die.roll }
-        die.rolls.to_s.should == die.rolls.collect { |roll| roll.value }.join(",")
-      end
-    end
+  it "should not allow unsupported formats" do
+    expect { Rollables::Die.new([1,2,:d]) }.to raise_exception
+    expect { Rollables::Die.new([1,2,[]]) }.to raise_exception
+    expect { Rollables::Die.new([1,2,{}]) }.to raise_exception
+    expect { Rollables::Die.new([1,2,Object.new]) }.to raise_exception
+  end
+end
+
+describe Rollables::DieFaces do
+  it "should allow valid DieFace formats" do
+    Rollables::Die.new([1,"b2"]).faces.should be_an_instance_of(Rollables::DieFaces)
   end
   
-  describe Rollables::DieFace do
-    it "should return the correct modified instance" do
-      die = Rollables::Die.new([1,"2","z"])
-      die.faces[0].should be_a(Integer)
-      die.faces[1].should be_a(Integer)
-      die.faces[2].should be_a(String)
-    end
-
-    it "should not allow unsupported formats" do
-      expect { Rollables::Die.new([1,2,:d]) }.to raise_exception
-      expect { Rollables::Die.new([1,2,[]]) }.to raise_exception
-      expect { Rollables::Die.new([1,2,{}]) }.to raise_exception
-      expect { Rollables::Die.new([1,2,Object.new]) }.to raise_exception
-    end
-  end
-
-  describe Rollables::DieFaces do
-    it "should allow valid DieFace formats" do
-      Rollables::Die.new([1,"b2"]).faces.should be_an_instance_of(Rollables::DieFaces)
-    end
-    
-    it "should not allow invalid DieFace formats" do
-      expect { Rollables::Die.new([1,2,:d]) }.to raise_exception
-      expect { Rollables::Die.new([1,2,[]]) }.to raise_exception
-      expect { Rollables::Die.new([1,2,{}]) }.to raise_exception
-      expect { Rollables::Die.new([1,2,Object.new]) }.to raise_exception
-    end
-    
-    it "should return a properly formatted string from to_s" do
-      [Rollables::Die.new(1..6), Rollables::Die.new(:d8), Rollables::Die.new([2,3,4,5]), Rollables::Die.new([1,"3","a","hi"])].each do |die|
-        die.faces.to_s.should == die.faces.join(",")
-      end
-    end
-
-    it "should be numeric if all faces have numeric values" do
-      Rollables::Die.new(6).faces.numeric?.should be_true
-      Rollables::Die.new([2,88,54,12,6]).faces.numeric?.should be_true
-      Rollables::Die.new(["6","2","487"]).faces.numeric?.should be_true
-    end
-
-    it "should not be numeric if any faces have non-numeric values" do
-      Rollables::Die.new(["a","b","c"]).faces.numeric?.should be_false
-    end
-
-    it "should be sequential if all faces are numeric and sequential" do
-      Rollables::Die.new(6).faces.sequential?.should be_true
-      Rollables::Die.new(:d6).faces.sequential?.should be_true
-      Rollables::Die.new([1,2,3]).faces.sequential?.should be_true
-      Rollables::Die.new(["7","8","9"]).faces.sequential?.should be_true
-    end
-
-    it "should not be sequential if any faces are non-numeric" do
-      Rollables::Die.new(["a","b","c"]).faces.sequential?.should be_false
-      Rollables::Die.new([1,2,3,"a"]).faces.sequential?.should be_false
-    end
-    
-    it "should not be sequential if any faces are non-sequential" do
-      Rollables::Die.new([1,3,5,7,9]).faces.sequential?.should be_false
-    end
-
-    it "should be simple if all faces are numeric and sequential and the low value is 1" do
-      Rollables::Die.new(:d6).faces.simple?.should be_true
-      Rollables::Die.new(1..3).faces.simple?.should be_true
-    end
-
-    it "should not be simple if any faces are non-numeric" do
-      Rollables::Die.new(["a","b","c"]).faces.simple?.should be_false
-      Rollables::Die.new([1,2,3,"a"]).faces.simple?.should be_false
-    end
-
-    it "should not be simple if any faces are non-sequential" do
-      Rollables::Die.new([2,5,9]).faces.simple?.should be_false
-    end
-
-    it "should not be simple if the low value is not 1" do
-      Rollables::Die.new(2..7).faces.simple?.should be_false
-    end
-
-    it "should be common if it is a a sequential 6-sided die with face values of 1-6" do
-      Rollables::Die.new("1d6").faces.common?.should be_true
-      Rollables::Die.new(1..6).faces.common?.should be_true
-    end
-
-    it "should only be common if it is a sequential 6-sided die with face values of 1-6" do
-      Rollables::Die.new(5).faces.common?.should be_false
-      Rollables::Die.new([1,3,4,5,6,7]).faces.common?.should be_false
-      Rollables::Die.new([2,3,4,5,6,7]).faces.common?.should be_false
-    end
-
-    it "should return the correct face value for high" do
-      Rollables::Die.new(20).faces.high.should == 20
-      Rollables::Die.new([7,2,9,5,4]).faces.high.should == 9
-      Rollables::Die.new(["a","b","c"]).faces.high.should == "c"
-      Rollables::Die.new(["c","b","a"]).faces.high.should == "a"
-    end
-
-    it "should return the correct face value for low" do
-      Rollables::Die.new(20).faces.low.should == 1
-      Rollables::Die.new([7,2,9,5,4]).faces.low.should == 2
-      Rollables::Die.new(["a","b","c"]).faces.low.should == "a"
-      Rollables::Die.new(["b","c","a"]).faces.low.should == "b"
-    end
-
-    it "should sort numeric faces when comparing high/low" do
-      Rollables::Die.new([7,2,9,5,4]).faces.high.should == 9
-      Rollables::Die.new([7,2,9,5,4]).faces.low.should == 2
-    end
-
-    it "should leave order intact for non-numeric faces when comparing high/low" do
-      Rollables::Die.new(["a","b","c"]).faces.high.should == "c"
-      Rollables::Die.new(["a","b","c"]).faces.low.should == "a"
-      Rollables::Die.new(["b","c","a"]).faces.high.should == "a"
-      Rollables::Die.new(["b","c","a"]).faces.low.should == "b"
-    end
+  it "should not allow invalid DieFace formats" do
+    expect { Rollables::Die.new([1,2,:d]) }.to raise_exception
+    expect { Rollables::Die.new([1,2,[]]) }.to raise_exception
+    expect { Rollables::Die.new([1,2,{}]) }.to raise_exception
+    expect { Rollables::Die.new([1,2,Object.new]) }.to raise_exception
   end
   
+  it "should return a properly formatted string from to_s" do
+    [Rollables::Die.new(1..6), Rollables::Die.new(:d8), Rollables::Die.new([2,3,4,5]), Rollables::Die.new([1,"3","a","hi"])].each do |die|
+      die.faces.to_s.should == die.faces.join(",")
+    end
+  end
+
+  it "should be numeric if all faces have numeric values" do
+    Rollables::Die.new(6).faces.numeric?.should be_true
+    Rollables::Die.new([2,88,54,12,6]).faces.numeric?.should be_true
+    Rollables::Die.new(["6","2","487"]).faces.numeric?.should be_true
+  end
+
+  it "should not be numeric if any faces have non-numeric values" do
+    Rollables::Die.new(["a","b","c"]).faces.numeric?.should be_false
+  end
+
+  it "should be sequential if all faces are numeric and sequential" do
+    Rollables::Die.new(6).faces.sequential?.should be_true
+    Rollables::Die.new(:d6).faces.sequential?.should be_true
+    Rollables::Die.new([1,2,3]).faces.sequential?.should be_true
+    Rollables::Die.new(["7","8","9"]).faces.sequential?.should be_true
+  end
+
+  it "should not be sequential if any faces are non-numeric" do
+    Rollables::Die.new(["a","b","c"]).faces.sequential?.should be_false
+    Rollables::Die.new([1,2,3,"a"]).faces.sequential?.should be_false
+  end
+  
+  it "should not be sequential if any faces are non-sequential" do
+    Rollables::Die.new([1,3,5,7,9]).faces.sequential?.should be_false
+  end
+
+  it "should be simple if all faces are numeric and sequential and the low value is 1" do
+    Rollables::Die.new(:d6).faces.simple?.should be_true
+    Rollables::Die.new(1..3).faces.simple?.should be_true
+  end
+
+  it "should not be simple if any faces are non-numeric" do
+    Rollables::Die.new(["a","b","c"]).faces.simple?.should be_false
+    Rollables::Die.new([1,2,3,"a"]).faces.simple?.should be_false
+  end
+
+  it "should not be simple if any faces are non-sequential" do
+    Rollables::Die.new([2,5,9]).faces.simple?.should be_false
+  end
+
+  it "should not be simple if the low value is not 1" do
+    Rollables::Die.new(2..7).faces.simple?.should be_false
+  end
+
+  it "should be common if it is a a sequential 6-sided die with face values of 1-6" do
+    Rollables::Die.new("1d6").faces.common?.should be_true
+    Rollables::Die.new(1..6).faces.common?.should be_true
+  end
+
+  it "should only be common if it is a sequential 6-sided die with face values of 1-6" do
+    Rollables::Die.new(5).faces.common?.should be_false
+    Rollables::Die.new([1,3,4,5,6,7]).faces.common?.should be_false
+    Rollables::Die.new([2,3,4,5,6,7]).faces.common?.should be_false
+  end
+
+  it "should return the correct face value for high" do
+    Rollables::Die.new(20).faces.high.should == 20
+    Rollables::Die.new([7,2,9,5,4]).faces.high.should == 9
+    Rollables::Die.new(["a","b","c"]).faces.high.should == "c"
+    Rollables::Die.new(["c","b","a"]).faces.high.should == "a"
+  end
+
+  it "should return the correct face value for low" do
+    Rollables::Die.new(20).faces.low.should == 1
+    Rollables::Die.new([7,2,9,5,4]).faces.low.should == 2
+    Rollables::Die.new(["a","b","c"]).faces.low.should == "a"
+    Rollables::Die.new(["b","c","a"]).faces.low.should == "b"
+  end
+
+  it "should sort numeric faces when comparing high/low" do
+    Rollables::Die.new([7,2,9,5,4]).faces.high.should == 9
+    Rollables::Die.new([7,2,9,5,4]).faces.low.should == 2
+  end
+
+  it "should leave order intact for non-numeric faces when comparing high/low" do
+    Rollables::Die.new(["a","b","c"]).faces.high.should == "c"
+    Rollables::Die.new(["a","b","c"]).faces.low.should == "a"
+    Rollables::Die.new(["b","c","a"]).faces.high.should == "a"
+    Rollables::Die.new(["b","c","a"]).faces.low.should == "b"
+  end
+end
+
+describe Rollables::DieRoll do
+  it "should roll on init" do
+    die = Rollables::Die.new(:d6)
+    roll = die.roll
+    roll.value.should_not be_nil
+    roll.timestamp.should_not be_nil
+  end
+
+  it "should return a properly formatted string from to_s" do
+    [Rollables::Die.new(6), Rollables::Die.new(["a","b","c","hello"])].each do |die|
+      20.times do
+        roll = die.roll
+        roll.to_s.should == roll.value.to_s
+      end
+    end
+  end
+end
+
+describe Rollables::DieRolls do
+  it "should return a properly formatted string from to_s" do
+    [Rollables::Die.new(6), Rollables::Die.new(["z","y","x"])].each do |die|
+      5.times { die.roll }
+      die.rolls.to_s.should == die.rolls.collect { |roll| roll.value }.join(",")
+    end
+  end
 end
